868ecad14231a67f2e8140b092924cd523694324
admin <aadhameltholth@gmail.com>
2026-02-28 04:03:52 +0200

    Fix email port and add try catch

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,13 +2,15 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
+});
 
   await transporter.sendMail({
     from: `"Darwish Car Check" <${process.env.EMAIL_USER}>`,
